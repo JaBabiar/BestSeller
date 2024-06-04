addf338da131e2d8dd4ba042c255f720fcb6e67a
ml3ko <58952380+ml3ko@users.noreply.github.com>
2024-06-04 14:16:39 +0200

    Update cards.class.php

--- classes/cards.class.php
+++ classes/cards.class.php
@@ -1,7 +1,15 @@
 <?php
 
-class card extends dbh{
+class Cards extends Dbh{
 
+    protected function getCards($panel){
+        $sql = 'SELECT * FROM cards where panel = ?';
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute([$panel]);
+
+        $results = $stmt->fetchAll();
+        return $results;
+    }
 
 }
 
